49e0ec8c311a02b9f879114737d0e2d7ea87b3cb
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-25 17:41:56 -0700

    pass tests source https://github.com/ericwongcv/AA-V2/blob/5d53afa8dcb35001d9a67acac1888141fd4db534/practice-for-week-04-custom-error-types/error-types/validation-error.js

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,15 @@
 // Your code here
+class ValidationError extends Error {
+  constructor(message = 'Invalid input') {
+    super(message);
+    this.message = message;
+    this.name = 'ValidationError';
+    // https://github.com/ericwongcv/AA-V2/blob/5d53afa8dcb35001d9a67acac1888141fd4db534/practice-for-week-04-custom-error-types/error-types/validation-error.js
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
